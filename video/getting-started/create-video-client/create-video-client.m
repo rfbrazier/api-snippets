432a729af0f88db73801f256ae7a5dbac6aaad7a
Rob Brazier <rob.brazier@gmail.com>
2016-10-14 14:44:39 -0700

    Update create-video-client.m

--- video/getting-started/create-video-client/create-video-client.m
+++ video/getting-started/create-video-client/create-video-client.m
@@ -1,1 +1,3 @@
+// Generate your own `accessToken` in the Twilio Console here: https://www.twilio.com/console/video/dev-tools/testing-tools
+
 TVIVideoClient *videoClient = [TVIVideoClient clientWithToken:accessToken];
