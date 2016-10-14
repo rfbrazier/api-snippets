5a880b2a9ad7b69d85107a83e148ad4ec4466e44
Rob Brazier <rob.brazier@gmail.com>
2016-10-14 14:39:18 -0700

    Add comments for readability

--- video/getting-started/create-video-client/create-video-client.js
+++ video/getting-started/create-video-client/create-video-client.js
@@ -6,5 +6,6 @@
 //
 //     const Video = Twilio.Video;
 //
+// Generate your own `accessToken` in the Twilio Console here: https://www.twilio.com/console/video/dev-tools/testing-tools
 
 const client = new Video.Client(accessToken);
